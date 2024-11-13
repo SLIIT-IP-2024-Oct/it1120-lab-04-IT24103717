4e05612970e80d88cd2f56925099a06ba217d43a
IT24103717 <it24103717@my.sliit.lk>
2024-11-13 11:08:40 +0530

    Add files via upload
    IT24103717Lab4Q1 Submitted

---
+++ IT24103717Lab4Q1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IT24103717Lab4Q1{
+    public static void main(String[]args) {
+
+ Scanner input= new Scanner(System.in);
+
+System.out.print("Enter a number: ");
+double number = input.nextDouble();
+
+if (number > 0) {
+    System.out.print("The number is: Positive");
+}
+
+    else if (number < 0) {
+    System.out.print("the number is: Negative");
+}
+else {
+     System.out.print("The number is Zero");
+    }
+
+}
+}
